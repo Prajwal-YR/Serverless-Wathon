d3b5d10dd133b353840ac2be8f63a116472f24d3
Prajwal-YR <yr.prajwal@gmail.com>
2022-11-16 23:22:17 -0800

    Server compiler updates

--- server-compiler/index.ts
+++ server-compiler/index.ts
@@ -4,7 +4,7 @@ import { TypeEnv } from "./ast";
 import { typeCheckProgram, } from "./typecheck";
 import { codeGenProgram } from "./codeGen";
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 type Post = {
@@ -28,28 +28,36 @@ function getJSONDataFromRequestStream<T>(request: IncomingMessage): Promise<T> {
 }
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  getJSONDataFromRequestStream<Post>(req).then(body => {
-    console.log("Body:", body);
-    //parse
-    const parsed = parse(body.program);
-    console.log("Parsed:", parsed);
-    //tc
-    const ProgramEnv: TypeEnv = {
-      vars: new Map(),
-      funcs: new Map(),
-      classes: new Map(),
-      retType: "None",
-    };
-    const typedAst = typeCheckProgram(parsed, ProgramEnv);
-    console.log("typed program", typedAst)
-    //codegen
-    const result = codeGenProgram(typedAst, ProgramEnv);
-    console.log("after codegen commands", result);
-    console.log("\n-------------------------------------------------------------------------------------------------------------------------\n");
+  if (req.method == 'POST') {
+    getJSONDataFromRequestStream<Post>(req).then(body => {
+      console.log("Body:", body);
+      //parse
+      const parsed = parse(body.program);
+      console.log("Parsed:", parsed);
+      //tc
+      const ProgramEnv: TypeEnv = {
+        vars: new Map(),
+        funcs: new Map(),
+        classes: new Map(),
+        retType: "None",
+      };
+      const typedAst = typeCheckProgram(parsed, ProgramEnv);
+      console.log("typed program", typedAst)
+      //codegen
+      const result = codeGenProgram(typedAst, ProgramEnv);
+      console.log("after codegen commands", result);
+      console.log("\n-------------------------------------------------------------------------------------------------------------------------\n");
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(result);
+    });
+  }
+  else if (req.method === 'GET') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(result);
-  });
+    //   console.log(req)
+    res.end('Wathon server compiler');
+  }
 });
 
 server.listen(port, hostname, () => {
